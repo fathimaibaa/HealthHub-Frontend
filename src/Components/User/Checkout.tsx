8965fe50f46d486afdfe05ecfd99f75698f00e19
hibaa <fathimaibaa@gmail.com>
2025-09-15 16:52:24 +0530

    (success page)

--- src/Components/User/Checkout.tsx
+++ src/Components/User/Checkout.tsx
@@ -80,7 +80,7 @@ const Checkout: React.FC = () => {
 
 
         Navigate({
-          to: `${"https://healthhubapp.online"}/payment_status/${bookingId}?success=true`,
+          to: `${"https://health-hub-frontend.vercel.app/"}/payment_status/${bookingId}?success=true`,
         });
 
       } else {
